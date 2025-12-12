cf7e7b7e5027682ba5a87ffc3913cd1a87f8972f
Jathavaan Shankarr <jathavaan12@gmail.com>
2025-12-12 10:35:01 +0100

    #1 Configured basic project structure

---
+++ src/config.py
@@ -0,0 +1,8 @@
+﻿from dataclasses import dataclass
+from pathlib import Path
+
+
+@dataclass(frozen=True)
+class Config:
+    INPUT_DATA_DIR: Path = Path.cwd() / "data" / "input"
+    OUTPUT_DATA_DIR: Path = Path.cwd() / "data" / "output"
