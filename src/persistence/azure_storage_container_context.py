7ae1b65e9fecb38fe4f33ba521868e86da679077
Jathavaan Shankarr <jathavaan12@gmail.com>
2025-12-17 10:37:38 +0100

    #3 Added persistence connection

---
+++ src/persistence/azure_storage_container_context.py
@@ -0,0 +1,8 @@
+﻿from azure.storage.blob import BlobServiceClient
+
+from src import Config
+
+
+def create_blob_storage_context() -> BlobServiceClient:
+    blob_storage_context = BlobServiceClient.from_connection_string(Config.AZURE_STORAGE_ACCOUNT_CONNECTION_STRING)
+    return blob_storage_context
--- src/persistence/duckdb_context.py
+++ src/persistence/duckdb_context.py
@@ -5,8 +5,12 @@ def create_duckdb_context() -> duckdb.DuckDBPyConnection:
     db_context: duckdb.DuckDBPyConnection = duckdb.connect()
     db_context.install_extension("spatial")
     db_context.load_extension("spatial")
+    db_context.install_extension("delta")
+    db_context.load_extension("delta")
     db_context.install_extension("azure")
     db_context.load_extension("azure")
+    db_context.install_extension("httpfs")
+    db_context.load_extension("httpfs")
 
     db_context.execute("""
         CREATE OR REPLACE SECRET azure_secret(
